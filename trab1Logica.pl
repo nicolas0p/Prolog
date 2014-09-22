2a0283c23c581007b9eb0a2c66a3d8bf55b2c729
Nicolas Pfeifer <nicolas0pfeifer@gmail.com>
2014-09-22 16:00:54 -0300

    Quantos professores não orientam graduação funcionando

--- trab1Logica.pl
+++ trab1Logica.pl
@@ -1,34 +1,31 @@
-%orientou(Professor, Aluno, modalidade). modalidade = ic | tcc | mestrado | doutorado
+%orientou(Professor, Aluno, modalidade). modalidade = graduacao | mestrado | doutorado
 %publicou(Professor, NomeDoArtigo, Ano).
 
 %Jerusa Marchi
 orientou('Jerusa Marchi', 'Jhonatan Alves', mestrado).
 orientou('Jerusa Marchi', 'Eduardo Burgel', mestrado).
 orientou('Jerusa Marchi', 'Darlan Anschau', mestrado).
-orientou('Jerusa Marchi', 'Joao Rogerio Vieira Neto', tcc).
-orientou('Jerusa Marchi', 'Leo Vieira Peres', tcc).
-orientou('Jerusa Marchi', 'Diego Gomes Couto', tcc).
-orientou('Jerusa Marchi', 'Mathias Antonio Mortari', tcc).
-orientou('Jerusa Marchi', 'Thiago Berti Thomaz', tcc).
-orientou('Jerusa Marchi', 'Mariana Tuma de Athayde', tcc).
-orientou('Jerusa Marchi', 'Augusto Fredigo Hack', tcc).
-orientou('Jerusa Marchi', 'Joao Guilherme Zeni', tcc).
-orientou('Jerusa Marchi', 'Bianca Seemann', tcc).
+orientou('Jerusa Marchi', 'Joao Rogerio Vieira Neto', graduacao).
+orientou('Jerusa Marchi', 'Leo Vieira Peres', graduacao).
+orientou('Jerusa Marchi', 'Diego Gomes Couto', graduacao).
+orientou('Jerusa Marchi', 'Mathias Antonio Mortari', graduacao).
+orientou('Jerusa Marchi', 'Thiago Berti Thomaz', graduacao).
+orientou('Jerusa Marchi', 'Mariana Tuma de Athayde', graduacao).
+orientou('Jerusa Marchi', 'Augusto Fredigo Hack', graduacao).
+orientou('Jerusa Marchi', 'Joao Guilherme Zeni', graduacao).
+orientou('Jerusa Marchi', 'Bianca Seemann', graduacao).
 orientou('Jerusa Marchi', 'Guilherme Klein Bitencourt', mestrado).
-orientou('Jerusa Marchi', 'Jhonatan Alves', tcc).
-orientou('Jerusa Marchi', 'Thiago Royer', ic).
+orientou('Jerusa Marchi', 'Jhonatan Alves', graduacao).
+orientou('Jerusa Marchi', 'Thiago Royer', graduacao).
 
-
-publicou('Jerusa Marchi', 'TrustE - An Emotional Trust Model for Agents', 2013).
-publicou('Jerusa Marchi', 'Relevant Minimal Change in Belief Update', 2013).
+publicou('Jerusa Marchi', 'TrustE - An Emotional Trust Model for Agents').
 
 
 %Luiz Claudio Villar dos Santos
-orientou('Luiz Claudio Villar dos Santos', 'Olav Philipp Henschel').
-orientou('Luiz Claudio Villar dos Santos', 'Vinicius dos Santos Livramento').
-orientou('Luiz Claudio Villar dos Santos', 'Emilio Wuerges').
-orientou('Luiz Claudio Villar dos Santos', 'Rafael Westphal').
-
+orientou('Luiz Claudio Villar dos Santos', 'Olav Philipp Henschel', mestrado).
+orientou('Luiz Claudio Villar dos Santos', 'Vinicius dos Santos Livramento', doutorado).
+orientou('Luiz Claudio Villar dos Santos', 'Emilio Wuerges', doutorado).
+orientou('Luiz Claudio Villar dos Santos', 'Rafael Westphal', doutorado).
 
 publicou('Luiz Claudio Villar dos Santos', 'On-the-fly Verification of Memory Consistency with Concurrent Relaxed Scoreboards').
 publicou('Luiz Claudio Villar dos Santos', 'Reconciling real-time guarantees and energy efficiency through unlocked-cache prefetching').
@@ -36,30 +33,75 @@ publicou('Luiz Claudio Villar dos Santos', 'Pre-silicon verification of multipro
 
 
 %Mario Antonio Ribeiro Dantas
-orientou('Mario Antonio Ribeiro Dantas', 'Eduardo Camilo Inácio').
-orientou('Mario Antonio Ribeiro Dantas', 'Izaias de Faria').
-orientou('Mario Antonio Ribeiro Dantas', 'Madalena Pereira da Silva').
-orientou('Mario Antonio Ribeiro Dantas', 'André Luiz Tinassi D\'Amato').
-orientou('Mario Antonio Ribeiro Dantas', 'Ramon Hugo de Souza').
-orientou('Mario Antonio Ribeiro Dantas', 'Eliza Helena Areias Gomes').
-orientou('Mario Antonio Ribeiro Dantas', 'Caio César de Melo e Silva').
-orientou('Mario Antonio Ribeiro Dantas', 'Douglas Dyllon Jeronimo de Macedo').
-orientou('Mario Antonio Ribeiro Dantas', 'João Carlos de Lima').
-orientou('Mario Antonio Ribeiro Dantas', 'Cristhian Pablo Machado').
-orientou('Mario Antonio Ribeiro Dantas', 'Eduardo Camilo Inácio').
-orientou('Mario Antonio Ribeiro Dantas', 'Richard Pereira e Silva').
-orientou('Mario Antonio Ribeiro Dantas', 'Matheus Rosado Vill').
-
+orientou('Mario Antonio Ribeiro Dantas', 'Eduardo Camilo Inacio', mestrado).
+orientou('Mario Antonio Ribeiro Dantas', 'Izaias de Faria', mestrado).
+orientou('Mario Antonio Ribeiro Dantas', 'Madalena Pereira da Silva', doutorado).
+orientou('Mario Antonio Ribeiro Dantas', 'Andre Luiz Tinassi D\'Amato', doutorado).
+orientou('Mario Antonio Ribeiro Dantas', 'Ramon Hugo de Souza', doutorado).
+orientou('Mario Antonio Ribeiro Dantas', 'Debora Cabral Nazario', doutorado).
+orientou('Mario Antonio Ribeiro Dantas', 'Eliza Helena Areias Gomes', mestrado).
+orientou('Mario Antonio Ribeiro Dantas', 'Caio Cesar de Melo e Silva', mestrado).
+orientou('Mario Antonio Ribeiro Dantas', 'Douglas Dyllon Jeronimo de Macedo', doutorado).
+orientou('Mario Antonio Ribeiro Dantas', 'Joo Carlos de Lima', doutorado).
+orientou('Mario Antonio Ribeiro Dantas', 'Cristhian Pablo Machado', graduacao).
+orientou('Mario Antonio Ribeiro Dantas', 'Eduardo Camilo Inacio', graduacao).
+orientou('Mario Antonio Ribeiro Dantas', 'Richard Pereira e Silva', graduacao).
+orientou('Mario Antonio Ribeiro Dantas', 'Matheus Rosado Vill', graduacao).
 
 publicou('Mario Antonio Ribeiro Dantas', 'A Survey into Performance and Energy Efficiency in HPC, Cloud and Big Data Environments').
+publicou('Mario Antonio Ribeiro Dantas', 'Context Management: Toward Assessing Quality of Context Parameters in a Ubiquitous Ambient Assisted Living Environment').
 publicou('Mario Antonio Ribeiro Dantas', 'Optimization of PACS Data Persistency Using Indexed Hierarchical Data').
-publicou('Mario Antonio Ribeiro Dantas', 'UBUNTU: A escola necessária para o futuro').
+publicou('Mario Antonio Ribeiro Dantas', 'UBUNTU: A escola necessaria para o futuro').
 publicou('Mario Antonio Ribeiro Dantas', 'A case-comparison study of automatic document classification utilizing both serial and parallel approaches').
 publicou('Mario Antonio Ribeiro Dantas', 'Modeling a distributed environment for a petroleum reservoir engineering application with software product line').
 publicou('Mario Antonio Ribeiro Dantas', 'Towards a Resource Reservation Approach for an Opportunistic Computing Environment').
 publicou('Mario Antonio Ribeiro Dantas', 'Green Cloud Computing: An Experimental Validation').
 publicou('Mario Antonio Ribeiro Dantas', 'A Data Management Study in a Private Cloud Storage').
 
+
+%Lau Cheuk Lung
+orientou('Lau Cheuk Lung', 'Daniel Presser', mestrado).
+orientou('Lau Cheuk Lung', 'Leandro Quibem Magnabosco', mestrado).
+orientou('Lau Cheuk Lung', 'Paulo Rogerio de Pinho Filho', mestrado).
+orientou('Lau Cheuk Lung', 'Paulo Henrique de Morais', mestrado).
+orientou('Lau Cheuk Lung', 'Tulio Alberton Ribeiro', mestrado).
+orientou('Lau Cheuk Lung', 'Rick Lopes de Souza', mestrado).
+orientou('Lau Cheuk Lung', 'Fernando Achylles Dettoni', doutorado).
+orientou('Lau Cheuk Lung', 'Hylson Vescovi Netto', doutorado).
+orientou('Lau Cheuk Lung', 'Alex Pinho Magalhes', doutorado).
+orientou('Lau Cheuk Lung', 'Aldelir Fernando Luiz', doutorado).
+orientou('Lau Cheuk Lung', 'Marcelo Ribeiro Xavier da Silva', mestrado).
+orientou('Lau Cheuk Lung', 'Fernando Achylles Dettoni', mestrado).
+
+publicou('Lau Cheuk Lung', 'MITRA: Byzantine Fault-Tolerant Middleware for Transaction Processing on Replicated Databases').
+publicou('Lau Cheuk Lung', 'Co-Evolution of Antagonistic Intelligent Agents Using Genetic Algorithms').
+publicou('Lau Cheuk Lung', 'Vbam - Byzantine Atomic Multicast in LAN Based on Virtualization Technology').
+publicou('Lau Cheuk Lung', 'Efficient Byzantine Fault-Tolerance').
+publicou('Lau Cheuk Lung', 'Using Virtualization Technology for Fault-Tolerant Replication in LAN').
+
+
+%Renato Fileto
+
+orientou('Renato Fileto', 'Juarez ångelo Piazza Sacenti', mestrado).
+orientou('Renato Fileto', 'Renata de Jesus Silva', mestrado).
+orientou('Renato Fileto', 'Ricardo Gil Belther Nabo', mestrado).
+orientou('Renato Fileto', 'Felipe Pinto da Silva', mestrado).
+orientou('Renato Fileto', 'Rafael Schmidt Melchert', graduacao).
+orientou('Renato Fileto', 'Carlos Augusto Schlyter Rodrigues', graduacao).
+orientou('Renato Fileto', 'Suelen Cardoso Fenali', graduacao).
+orientou('Renato Fileto', 'Sergio Zarth Junior', graduacao).
+orientou('Renato Fileto', 'Lucas Scharf', graduacao).
+orientou('Renato Fileto', 'Lucas Pereira Zarbato', graduacao).
+orientou('Renato Fileto', 'Douglas Klein', graduacao).
+orientou('Renato Fileto', 'Cleto May', graduacao).
+orientou('Renato Fileto', 'Karina Fasolin', mestrado).
+orientou('Renato Fileto', 'Andre Salvaro Furtado', mestrado).
+orientou('Renato Fileto', 'Juarez ångelo Piazza Sacenti', graduacao).
+orientou('Renato Fileto', 'Marcelo Kruger', graduacao).
+orientou('Renato Fileto', 'Jean Goncalves', graduacao).
+
+publicou('Renato Fileto', 'SMoT+: Extending the SMoT Algorithm for Discovering Stops in Nested Sites').
+publicou('Renato Fileto', 'Assessing the Attractiveness of Places with Movement Data').
 
 
 %1. Qual e o orientador de um dado aluno? 
@@ -76,8 +118,11 @@ quantosAlunos(Prof, Quantidade) :- todosOsAlunos(Prof, Lista), length(Lista, Qua
 publicacoes(Pessoa, Publicacoes) :- findall(Artigo, publicou(Pessoa, Artigo, _), Publicacoes).
 
 %6. Qual o professor (entre os k) com maior numero de orientacoes?
-professorComMaisOrientacoes(Maior) :- todosOsProfessores(Lista), maiorAlunosProfDaLista(Lista, Maior).
-maiorAlunosProfDaLista([Head|Lista], Prof) :- maiorProfessor(Head, Prof, Maior), maiorAlunosProfDaLista(Lista, Maior).
+%professorComMaisOrientacoes().
+
+	%Maior é o professor, entre Um e Dois, que tem maior numero de alunos
+maiorProfessor(Um, Dois, Um) :- quantosAlunos(Um, QuantUm), quantosAlunos(Dois, QuantDois), QuantUm >= QuantDois.
+maiorProfessor(Um, Dois, Dois) :- quantosAlunos(Um, QuantUm), quantosAlunos(Dois, QuantDois), QuantUm < QuantDois.
 
 %7. Quantas publicacoes de artigos cientificos um dado aluno (orientando) possui?
 quantasPublicacoes(Aluno, Quantidade) :- aluno(Aluno), publicacoes(Aluno, Lista), length(Lista, Quantidade).
@@ -86,17 +131,17 @@ quantasPublicacoes(Aluno, Quantidade) :- aluno(Aluno), publicacoes(Aluno, Lista)
 
 
 %9. Quantos professores nao orientam alunos de graduacao (trabalhos de conclusaao ou iniciacao cientifica)?
-naoOrientamGraduacao(NaoOrientam) :- length()
-naoOrientamGraduacao(NaoOrientam) :- todosOsProfessores(Todos), 
+naoOrientamGraduacao(Numero) :- todosOsProfessores(Todos), contarNaoGraduacao(Todos, 0, Numero).
+contarNaoGraduacao([], Numero, Numero).
+contarNaoGraduacao([Head|Professores], NaoOrientam, Final) :- findall(Aluno, orientou(Head, Aluno, graduacao), Graduandos), length(Graduandos, Tamanho), Tamanho is 0, Resposta is NaoOrientam + 1, contarNaoGraduacao(Professores, Resposta, Final).
+contarNaoGraduacao([Head|Professores], NaoOrientam, Final) :- findall(Aluno, orientou(Head, Aluno, graduacao), Graduandos), length(Graduandos, Tamanho), Tamanho > 0, contarNaoGraduacao(Professores, NaoOrientam, Final).
+
 
 %10. Qual a media de orientacooes por professor (entre os k)?
 mediaDeOrientacoes(Media) :- todosOsProfessores(Todos), somaDasOrientacoes(Todos, Soma), length(Todos, Tamanho), Media is Soma / Tamanho.
 
 %11.  Quantos estudantes tem chance de finalizar, em 2014, suas dissertac̃oes de mestrado? Suponha que um trabalho de mestrado leva, no mınimo, um ano e meio para ser concluıdo
 
-
-retirarNaoMestrandos(Todos, []) :- length(Todos, 0).
-retirarNaoMestrandos([Aluno|Resto], Mestrandos) :- 
 
 %12. Há algum artigo científico no qual dois ou mais professores(entre os k) são coautores? Quais?
 coautores(Professores, Artigo) :- false.
@@ -114,6 +159,3 @@ todosOsProfessores(Final) :- findall(Prof, professor(Prof), Lista), removeDuplic
 %Remove os duplicados da Lista e guarda em SemDuplicados
 removeDuplicados(Lista, SemDuplicados) :- sort(Lista, SemDuplicados).
 
-%Maior é o professor, entre Um e Dois, que tem maior numero de alunos
-maiorProfessor(Um, Dois, Um) :- quantosAlunos(Um, QuantUm), quantosAlunos(Dois, QuantDois), QuantUm >= QuantDois.
-maiorProfessor(Um, Dois, Dois) :- quantosAlunos(Um, QuantUm), quantosAlunos(Dois, QuantDois), QuantUm < QuantDois.
